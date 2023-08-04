1c179ea6581c21a81f721a525683b740c86e8f8c
hena98 <gjgksmf0411@gmail.com>
2023-08-04 18:12:50 +0900

    hena98 관리자유저페이지 추가

---
+++ src/main/java/com/kmong/kmongdemo/mapper/AdminUserMapper.java
@@ -0,0 +1,14 @@
+package com.kmong.kmongdemo.mapper;
+
+import com.kmong.kmongdemo.domain.UserDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface AdminUserMapper {
+
+    List<UserDTO> userLists();
+
+    UserDTO userInfo(String userId);
+}
