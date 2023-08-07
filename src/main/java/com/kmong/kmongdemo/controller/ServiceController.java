ffaf2bb597ff5b6e85d421e25a8d82328c370334
Boomin-pop <opr999@korea.ac.kr>
2023-08-08 00:28:35 +0900

    ServiceInput required option implemented and ui design modified on serviceInput page.

--- src/main/java/com/kmong/kmongdemo/controller/ServiceController.java
+++ src/main/java/com/kmong/kmongdemo/controller/ServiceController.java
@@ -11,10 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RequestMapping("/service")
 @Controller
@@ -32,20 +29,20 @@ public class ServiceController {
     }
 
     @PostMapping("/chkedServiceType")
-    public  @ResponseBody Map<String, String> chkedServiceType(@RequestBody CategoryDTO cDto, Model model){
+    public  @ResponseBody List<ServiceTypeDTO> chkedServiceType(@RequestBody CategoryDTO cDto, Model model){
 
         String code = cDto.getServiceTopCatCode();
           System.out.println("code = " + code);
           ServiceTypeChkDTO stcDTO = serviceService.serviceTypeChkList(code);
-            System.out.println("서비스타이프 체크된 코드들 = " + stcDTO);
+            System.out.println("서비스타입 체크된 코드들 = " + stcDTO);
             //List<ServiceTypeDTO> = serviceService.serviceTypeList();
             //Map<String, String> serviceTypeList = new HashMap<>();
-        Map<String, String> serviceTypeList = serviceService.serviceTypeList();
-
-         //       System.out.println("serviceTypeList = " + serviceTypeList);
+        //Map<String, String> serviceTypeList = serviceService.serviceTypeList();
+        List<ServiceTypeDTO> serviceTypeList = serviceService.serviceTypeList();
+        //System.out.println("serviceTypeList = " + serviceTypeList);
+        List<ServiceTypeDTO> chkedTypeList = new ArrayList<>();
         Map<String, String> chkedServiceType = new HashMap<>();
-
-            try {
+        try {
                 Object obj = stcDTO;
                 for (Field field : obj.getClass().getDeclaredFields()) {
                     field.setAccessible(true);
@@ -54,21 +51,56 @@ public class ServiceController {
                     if(objValue.equals(""))
                         break;
 
-                    System.out.println("key : " + key + ", value : " + objValue);
-
+                    System.out.println("chkDTO_key : " + key + ", chkDTO_value : " + objValue);
                     if(!key.equals("serviceTypeChkCode") && !objValue.equals("")){
-                 //       for(String typeKey: serviceTypeList.keySet()){
-                          for(Map.Entry<String, String> typeList : serviceTypeList.entrySet()){
-                              if(objValue.equals(typeList.getKey())){
-                                  chkedServiceType.put(typeList.getKey(), typeList.getValue());
-                                       }
-                                   }
+                        for(int i=0; i<serviceTypeList.size(); i++) {
+                            if (objValue.equals(serviceTypeList.get(i).getServiceTypeCode())){
+                           ServiceTypeDTO stDTO = new ServiceTypeDTO();
+                           stDTO.setServiceTypeCode(serviceTypeList.get(i).getServiceTypeCode());
+                           stDTO.setServiceTypeName(serviceTypeList.get(i).getServiceTypeName());
+                           stDTO.setMandatoryInput(serviceTypeList.get(i).getMandatoryInput());
+                           chkedTypeList.add(stDTO);
+                            }
                         }
-                    System.out.println("selectedType = " + chkedServiceType);
+                    }
+                    System.out.println("chkedTypeListDTO = " + chkedTypeList);
                 }
+
+
+        //       System.out.println("serviceTypeList = " + serviceTypeList);
+//        Map<String, String> chkedServiceType = new HashMap<>();
+//        Map<String, Integer> mandatoryInput = new HashMap<>();
+//            try {
+//                Object obj = stcDTO;
+//                for (Field field : obj.getClass().getDeclaredFields()) {
+//                    field.setAccessible(true);
+//                    Object key = field.getName();
+//                    Object objValue = field.get(obj);
+//                    if(objValue.equals(""))
+//                        break;
+//
+//                    System.out.println("key : " + key + ", value : " + objValue);
+//
+//                    if(!key.equals("serviceTypeChkCode") && !objValue.equals("")){
+//                 //       for(String typeKey: serviceTypeList.keySet()){
+//                          for(Map.Entry<String, String> typeList : serviceTypeList.entrySet()){
+//                              if(objValue.equals(typeList.getKey())) {
+//                                  chkedServiceType.put(typeList.getKey(), typeList.getValue());
+//                                  for (Map.Entry<String, Integer> mandatoryList : serviceTypeMandList.entrySet()) {
+//                                      if (typeList.getKey().equals(mandatoryList.getKey())) {
+//                                          mandatoryInput.put(mandatoryList.getKey(), mandatoryList.getValue());
+//                                      }
+//                                  }
+//                              }
+//                                   }
+//                        }
+//                    System.out.println("selectedType = " + chkedServiceType);
+//                    System.out.println("mandatoryInput = " + mandatoryInput);
+//                }
             }catch (Exception e){
                 e.printStackTrace();}
 
-        return chkedServiceType;
+//        return (List<ServiceTypeDTO>) chkedServiceType;
+        return (List<ServiceTypeDTO>) chkedTypeList;
     }
 }
