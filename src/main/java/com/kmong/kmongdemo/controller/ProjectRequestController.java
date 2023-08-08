29757b26c3a539c91229fbe65251831fa8be1517
DELPRO0814 <wogus3317@gmail.com>
2023-08-08 17:44:49 +0900

    프로젝트 생성 페이지(js링크가 안돼서 임시로 html안에 넣음), 프로젝트 요구사항 입력시 비어있는 항목이 있는지 유효성 검사 구현, 요구사항 입력시 자잘한 제한(글자수, 최소금액, 날자) 미구현, 프로젝트 생성 페이지로 가면 생성되어 있는 프로젝트 리스트 가져오기(출력은 아직 구현 안함), 프로젝트 업로드 구현, 프로젝트 테이블 생성 SQL문

---
+++ src/main/java/com/kmong/kmongdemo/controller/ProjectRequestController.java
@@ -0,0 +1,36 @@
+package com.kmong.kmongdemo.controller;
+
+import com.kmong.kmongdemo.domain.ProjectRequestDTO;
+import com.kmong.kmongdemo.service.ProjectRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectRequestController {
+    @Autowired
+    private ProjectRequestService prService;
+
+    @GetMapping("/request")
+    public String requestForm(HttpSession session, Model model) {
+        ArrayList<ProjectRequestDTO> list = prService.projectList(session.getId());
+
+        System.out.println("list = " + list);
+
+        return "projectRequest/projectRequest";
+    }
+
+    @PostMapping("/request")
+    public String request(String category, String title, String work, String requested, String budget, String wanted, String close, String term, HttpSession session) {
+        prService.uploadProject(category, title, work, requested, budget, wanted, close, term, session.getId());
+
+        return "redirect:/project/request";
+    }
+}
---
+++ src/main/resources/static/js/projectRequest.js
@@ -0,0 +1,22 @@
+    let first = $('#first').display.hide();
+    let second = $('#second').display.hide();
+    let third = $('#third').display.hide();
+    let fourth = $('#fourth').display.hide();
+    let fifth = $('#fifth').display.hide();
+    let end = $('#end').display.hide();
+    first.style.display = 'none';
+    second.style.display = 'none';
+    third.style.display = 'none';
+    fourth.style.display = 'none';
+    fifth.style.display = 'none';
+$('#nextBtn').on("click", function () {
+    alert("이벤트");
+});
+    category
+    title
+    work
+    requested
+    budget
+    wanted
+    close
+    term
