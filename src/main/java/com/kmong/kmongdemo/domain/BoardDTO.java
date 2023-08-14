c0361254690874b9ca28a9820faa26e9172ce29b
befly <beflygunwoo@naver.com>
2023-08-14 16:20:22 +0900

    게시글 등록 (이미지 등록까지) 삭제 완성

--- src/main/java/com/kmong/kmongdemo/domain/BoardDTO.java
+++ src/main/java/com/kmong/kmongdemo/domain/BoardDTO.java
@@ -1,6 +1,7 @@
 package com.kmong.kmongdemo.domain;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.Timestamp;
 
@@ -9,12 +10,16 @@ public class BoardDTO {
     private int boardId;
     private int boardSectionId;
     private String boardSectionName;
+    private int boardCategoryId;
     private String boardCategoryName;
     private String boardWriter;
     private Timestamp boardUploadTime;
     private String boardTitle;
     private String boardContent;
-    private UploadFile boardImg1;
-    private UploadFile boardImg2;
-    private UploadFile boardImg3;
+    private String boardImg1;
+    private String boardImg2;
+    private String boardImg3;
+    private MultipartFile imgFile1;
+    private MultipartFile imgFile2;
+    private MultipartFile imgFile3;
 }
