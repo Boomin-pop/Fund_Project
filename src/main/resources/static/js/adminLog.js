1850ceacd070e088480d38cb885bd56a73ad4a60
befly <beflygunwoo@naver.com>
2023-08-14 21:43:01 +0900

    로그 관리 유저 상세보기 기능 추가

--- src/main/resources/static/js/adminLog.js
+++ src/main/resources/static/js/adminLog.js
@@ -10,6 +10,28 @@ function pageMoveS(page){
     let id = document.getElementById("id").value;
     location.href="signLog?page=" + page + "&by=" + by + "&ud=" + ud + "&id=" + id;
 }
-function userView(id){
 
+let modal2 = document.getElementById("Modal2");
+function userView(userId){
+    $.ajax({
+        url: "/admin/user/"+userId,
+        type: "get",
+        success : function (data) {
+            modal2.style.display = "block";
+            console.log(data);
+            document.getElementById("name2").textContent = data.userName;
+            document.getElementById("id2").textContent = data.userId;
+            document.getElementById("pw2").textContent = data.userPassword;
+            document.getElementById("nick2").textContent = data.userNickname;
+            document.getElementById("email2").textContent = data.userEmail;
+            document.getElementById("tel2").textContent = data.userTel;
+            document.getElementById("job2").textContent = data.userJobId;
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+}
+function span2(){
+    modal2.style.display = "none";
 }
